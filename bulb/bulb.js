bcd3cbc4c5d348fcc1d42ce8b7bc4cae7760089a
shalini <shalini22@navgurukul.org>
2026-09-11 00:34:32 -0700

    first commit

---
+++ bulb/bulb.js
@@ -0,0 +1,16 @@
+
+ 
+
+function turnon(){
+    document.getElementById("image").innerHTML =
+    '<img src="on2.jpg" alt="bulb on ">'; 
+ 
+}
+
+function turnoff(){
+    document.getElementById("image").innerHTML =
+    '<img src="off2.jpg" alt="bulb off">';
+
+}
+
+ 
